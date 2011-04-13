55fb0ef702797e6b7b0a33be091372c9370029fc
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2011-04-13 18:04:35 +0200

    Much more elegant way of handling broadcast

--- ws.js
+++ ws.js
@@ -1,22 +1,20 @@
 var ws = require("websocket-server");
 
+var port = 8080;
 var server = ws.createServer();
-var connectionIds=[];
 
 function update() {
-    if (connectionIds.length) {
-        connectionIds.forEach(function(id) {
-            server.send(id,new Date());
-        });
-    }
+    server.broadcast(new Date());
     setTimeout(update, 1000);
-}
+};
 
 setTimeout(update, 1000);
 
 server.addListener("connection", function(connection) {
-    connection.send("hello " + connection.id);
-    connectionIds.push(connection.id); 
-  });
+    connection.send("Hello " + connection.id);
+    connection.send("There is " + server.manager.length + " clock watcher right now.");
+});
 
-server.listen(8080);
+
+server.listen(port);
+console.log("Clock Server started on port " + port);
